e4421caf6b8977b4cb9031c03760064380ddcd81
Sputnik Antolovich <inquiry@dermah.com>
2019-10-12 16:49:05 +1100

    Set manifests and themes

--- pulsar/lib/pulsar_web/endpoint.ex
+++ pulsar/lib/pulsar_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule PulsarWeb.Endpoint do
     at: "/",
     from: :pulsar,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt manifest.json)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
