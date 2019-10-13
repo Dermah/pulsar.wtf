4f5d80579314ce0835d547e68cd8190450e623d1
Sputnik Antolovich <inquiry@dermah.com>
2019-10-13 11:49:44 +1100

    Add mix release config

--- pulsar/build.sh
+++ pulsar/build.sh
@@ -8,7 +8,13 @@ MIX_ENV=prod mix compile
 npm run deploy --prefix ./assets
 mix phx.digest
 
-echo "  Done. To run:"
-echo "PORT=4001 MIX_ENV=prod mix phx.server"
-echo "  or detached:"
-echo "PORT=4001 MIX_ENV=prod elixir --erl \"-detached\" -S mix phx.server"
+MIX_ENV=prod mix release
+
+# Without mix release:
+# echo "  Done. To run:"
+# echo "PORT=4001 MIX_ENV=prod mix phx.server"
+# echo "  or detached:"
+# echo "PORT=4001 MIX_ENV=prod elixir --erl \"-detached\" -S mix phx.server"
+
+echo ""
+echo "Bundle at _build/prod/rel/pulsar"
--- pulsar/config/prod.secret.exs
+++ pulsar/config/prod.secret.exs
@@ -20,7 +20,7 @@ config :pulsar, PulsarWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :pulsar, PulsarWeb.Endpoint, server: true
+config :pulsar, PulsarWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
