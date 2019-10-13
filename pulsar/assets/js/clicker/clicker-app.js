28e907b7ed341082b5ea59573e8dec0d3985e341
Sputnik Antolovich <inquiry@dermah.com>
2019-10-13 15:19:27 +1100

    Make a nice clicker visualiser

--- pulsar/assets/js/clicker/clicker-app.js
+++ pulsar/assets/js/clicker/clicker-app.js
@@ -27,6 +27,8 @@ new p5(function(p) {
   let pulseSize;
   let canvas;
   let amp;
+  let fft;
+  let peakDetect;
 
   p.preload = function() {
     p.soundFormats("mp3");
@@ -34,8 +36,8 @@ new p5(function(p) {
   };
 
   p.setup = function() {
-    canvas = p.createCanvas(300, 300);
-    p.frameRate(24);
+    canvas = p.createCanvas(p.windowWidth, 300);
+    p.frameRate(60);
 
     var myDiv = p
       .createDiv("click to start")
@@ -62,6 +64,9 @@ new p5(function(p) {
         amp = new p5.Amplitude();
       }
     });
+
+    fft = new p5.FFT();
+    peakDetect = new p5.PeakDetect();
   };
 
   p.draw = function() {
@@ -72,7 +77,71 @@ new p5(function(p) {
 
     p.circle(0, 0, amp ? amp.getLevel() * p.width : 0);
 
-    amp && channel.push("click", { size: amp.getLevel() });
+    // peakDetect accepts an fft post-analysis
+    const spectrum = fft.analyze();
+    peakDetect.update(fft);
+
+    peakDetect.isDetected && channel.push("click", { size: amp.getLevel() });
+
+    p.push();
+    p.translate(-p.width / 2, -p.height / 2);
+
+    // Spectrum
+    p.noStroke();
+    p.fill(0, 255, 0); // spectrum is green
+    for (var i = 0; i < spectrum.length; i++) {
+      var x = p.map(i, 0, spectrum.length, 0, p.width);
+      var h = -p.height + p.map(spectrum[i], 0, 255, p.height, 0);
+      p.rect(x, p.height, p.width / spectrum.length, h);
+    }
+
+    // Energy Dials
+    p.fill(0, 0, 255);
+    p.rect(
+      (p.width / 5) * 0,
+      0,
+      ((fft.getEnergy("bass") / 255) * p.width) / 5,
+      10
+    );
+    p.rect(
+      (p.width / 5) * 1,
+      0,
+      ((fft.getEnergy("lowMid") / 255) * p.width) / 5,
+      10
+    );
+    p.rect(
+      (p.width / 5) * 2,
+      0,
+      ((fft.getEnergy("mid") / 255) * p.width) / 5,
+      10
+    );
+    p.rect(
+      (p.width / 5) * 3,
+      0,
+      ((fft.getEnergy("highMid") / 255) * p.width) / 5,
+      10
+    );
+    p.rect(
+      (p.width / 5) * 4,
+      0,
+      ((fft.getEnergy("treble") / 255) * p.width) / 5,
+      10
+    );
+
+    // Waveform
+    var waveform = fft.waveform();
+    p.noFill();
+    p.beginShape();
+    p.stroke(255, 0, 0); // waveform is red
+    p.strokeWeight(1);
+    for (var i = 0; i < waveform.length; i++) {
+      var x = p.map(i, 0, waveform.length, 0, p.width);
+      var y = p.map(waveform[i], -1, 1, 0, p.height);
+      p.vertex(x, y);
+    }
+    p.endShape();
+    p.pop();
+    // amp && channel.push("click", { size: amp.getLevel() });
     p.pop();
   };
 });
